9df60dadf43a5981f306f4cc4726f338fd1dd683
Jack Rau <98289666+jack14730@users.noreply.github.com>
2025-01-29 13:40:23 -0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+'''
+    Variables
+'''
+
+name = "Jack"
+age = 22
+tax = .045
+puppies = True
+pi = 3.1415
+
+print(name + " is " + str(age) + " years old")
+
+print(f"{name} is {age} years old.")
+
+print(f"{name} likes pie {pi:.2f}")
+print(f"{name} ooops. Lemon Meringue")
+
+print(f"What does DNA stand for")
+print("The National Dyslexic association")
