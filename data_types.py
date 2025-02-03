cbb62a2c84946382d7ee945f5837436835fc736b
Jack Rau <98289666+jack14730@users.noreply.github.com>
2025-02-03 02:20:22 -0600

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 47)
+
+# Integer
+y = -14
+print("Integer:", y)
+
+# Float
+z = 15.5
+print("Float:", z)
+
+# String
+name = "Jack"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
