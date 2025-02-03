de1d0fb6c4edcba12d1910d99c24d3a53db160e3
Jack Rau <98289666+jack14730@users.noreply.github.com>
2025-02-03 11:19:28 -0600

    Add files via upload

--- conversion.py
+++ conversion.py
@@ -12,10 +12,25 @@
     Example: 5 kg * 2.20462 = 11.0231 lb
 
 '''
-c = 2.20462
-kilo = 150
-pound = kilo * c
+# c = 2.20462
+# kilo = 150
+# pound = kilo * c
 
-print(f"{kilo} kilograms = {pound: .1f} pounds")
+# print(f"{kilo} kilograms = {pound: .1f} pounds")
 
+# variables
+kg_1 = 20
+kg_2 = 45
+kg_3 = 15
+kg_4 = 30
 
+conversion = 2.20462
+lb_1 = kg_1 * conversion
+lb_2 = kg_2 * conversion
+lb_3 = kg_3 * conversion
+lb_4 = kg_4 * conversion
+
+print(f"{kg_1} Kilograms is equal to {lb_1:.2f} pounds")
+print(f"{kg_2} Kilograms is equal to {lb_2:.2f} pounds")
+print(f"{kg_3} Kilograms is equal to {lb_3:.2f} pounds")
+print(f"{kg_4} Kilograms is equal to {lb_4:.2f} pounds")
