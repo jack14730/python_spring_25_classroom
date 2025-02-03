8accc195832abc8d44dc9f7334e8e9f4e1e7abea
Jack Rau <98289666+jack14730@users.noreply.github.com>
2025-02-03 11:19:17 -0600

    Add files via upload

---
+++ mathematical_operations.py
@@ -0,0 +1,9 @@
+# variables
+length = 10
+width = 30
+# calculate area
+area = length * width
+
+# print variable  values and calculate area
+print(f"The length is equal to {length} and the with is equal to {
+      width}, and the area of the two is equal to {area}")
