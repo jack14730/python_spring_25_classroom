f0a21a51f982fcc66d45f660fc7c917da62fe61d
Jack Rau <98289666+jack14730@users.noreply.github.com>
2025-02-05 06:48:45 -0600

    Add files via upload

--- budget_calculator.py
+++ budget_calculator.py
@@ -21,9 +21,12 @@
 """
 
 #     # variables
+
+# i am using this so I have numbers to type in for the inputs
+
 # total budget = 95000
 # net monthly income = 5780
-# money to spend: 2890  # money amount set aside to use for needs
+# money to spend = 2890  # money amount set aside to use for needs
 # rent = 1944  # rent per month
 # utilities = 335  # utility cost per month
 # groceries = 327.31
@@ -34,33 +37,47 @@
 # debt = 719
 
 # Get input from user
-budget = float(input("budget: 5780 "))
-rent = float(input(" rent per month: 1944 "))
-utilities = float(input(" utilities per month: 335 "))
-groceries = float(input(" groceries cost per month: 327.31 "))
-transportation = float(input(" transportation cost per month: 441.67 "))
-health_care = float(input(" health care per month: 279.49 "))
-clothing = float(input(" clothing per month: 120 "))
-debt = float(input("debt per month: 719"))
+
+budget = float(input("budget: "))
+rent = float(input("rent per month: "))
+utilities = float(input("utilities per month: "))
+groceries = float(input("groceries cost per month: "))
+transportation = float(input("transportation cost per month: "))
+health_care = float(input("health care per month: "))
+clothing = float(input("clothing per month: "))
+debt = float(input("debt per month: "))
+# this is to allow the user to input numbers into the terminal to allow the program to use those numbers for the calculations
+
 
 # calculations
 
-rent_percent = budget/rent * 100
-utilities_percent = budget/utilities * 100
-groceries_percent = budget/groceries * 100
-transportation_percent = budget/transportation * 100
-health_care_percent = budget/health_care * 100
-clothing_percent = budget/clothing * 100
-debt_percent = budget/debt * 100
+rent_percent = (rent / budget) * 100
+utilities_percent = (utilities / budget) * 100
+groceries_percent = (groceries / budget) * 100
+transportation_percent = (transportation / budget) * 100
+health_care_percent = (health_care / budget) * 100
+clothing_percent = (clothing / budget) * 100
+debt_percent = (debt / budget) * 100
 
 # total budget spent
 
-print(f"Your housing is %{rent_percent:.1f} of your monthly budget.")
-print(f"Your housing is %{utilities_percent:.1f} of your monthly budget.")
-print(f"Your housing is %{groceries_percent:.1f} of your monthly budget.")
-print(f"Your housing is %{transportation_percent:.1f} of your monthly budget.")
-print(f"Your housing is %{health_care_percent:.1f} of your monthly budget.")
-print(f"Your housing is %{clothing_percent:.1f} of your monthly budget.")
-print(f"Your housing is %{debt_percent:.1f} of your monthly budget.")
+print(f"Your rent is %{rent_percent:.1f} of your monthly budget.")
+print(f"Your utilities is %{utilities_percent:.1f} of your monthly budget.")
+print(f"Your groceries is %{groceries_percent:.1f} of your monthly budget.")
+print(f"Your transportation is %{
+      transportation_percent:.1f} of your monthly budget.")
+print(f"Your health_care is %{
+      health_care_percent:.1f} of your monthly budget.")
+print(f"Your clothing is %{clothing_percent:.1f} of your monthly budget.")
+print(f"Your debt is %{debt_percent:.1f} of your monthly budget.")
+# print of what your percentages will be for a monthly budget
+
 
 # Money left after expenses
+
+# calculates all of the expenses to give number of how much spent
+expenses = (rent + utilities + groceries +
+            transportation + health_care + clothing + debt)
+leftover_funds = (budget - expenses)  # calculates how much is left over
+# print of what you have left over after typing each expense in the terminal
+print("Your leftover available funds after expenses is: ", leftover_funds)
