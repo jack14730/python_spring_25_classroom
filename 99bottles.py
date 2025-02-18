2673392989f1bb539305443923b82ece33ec153b
Jack Rau <Jrau@students.mchenry.edu>
2025-02-17 23:07:52 -0600

    2/17

--- 99bottles.py
+++ 99bottles.py
@@ -2,18 +2,26 @@
 while loop demonstration
 
 '''
-
-# While
-condition = True
+# first i entered the count at 0, then i have the beer to equal 99
 count = 0
-while count <= 10:
-    print(count)
+beer = 99
+# I use a while statement to let it count down from 98
+while count <= 98:
+    if beer > 1:
+        print(f"{beer} bottles of beer on the wall!")
+        print(f"{beer} bottles of beer!")
+    else:
+        print(f"{beer} bottle of beer on the wall!")
+        print(f"{beer} bottle of beer!")
+# Then I have the elif statement and else statement for when the terminal reaches 0
+    beer -= 1
+    print("Take one down, pass it around")
+    if beer > 1:
+        print(f"{beer} bottles of beer on the wall!")
+    elif beer == 1:
+        print(f"{beer} bottle of beer on the wall!")
+    else:
+        print("No more bottles of beer on the wall!")
+
+    print("\n")
     count += 1
-
-# For with range
-for count in range(1, 13, 2):
-    print(count)
-
-# for with list
-for day in ("Sunday", "Monday", "Tuesday", "Wednesday"):
-    print(day)
