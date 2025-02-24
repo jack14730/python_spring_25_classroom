d4063ed78a76c29d577a730fec37fcc004b56be1
Jack Rau <Jrau@students.mchenry.edu>
2025-02-24 16:48:58 -0600

    2/24 WIP

---
+++ list_assignment.py
@@ -0,0 +1,24 @@
+"""
+  
+    Create a List: Start by creating a list named days that includes all seven days of the week.
+   
+    initialize an Empty List: Create an empty list called steps to store the number of steps taken each day.
+   
+    User Input: Using a loop, ask the user to enter the number of steps they took for each day.
+    
+    Store Steps: Append the user's input to the steps list.
+    
+    Display Daily Steps: Show the user the steps recorded for each day.
+    
+    Total Steps: Calculate and display the total number of steps taken in the week.
+    
+    Average Steps: Calculate and display the average steps.
+
+"""
+
+# variables
+
+# lists
+
+day = ["Sunday", "Monday", "Tuesday",
+       "Wednesday", "Thursday", "Friday", "Saturday"]
